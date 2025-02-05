cda6e5b35267a4423167b36845945985a4292d1d
“PororoAndFriends” <gbham0815@pukyong.ac.kr>
2025-02-06 01:13:00 +0900

    init files

--- src/main/java/com/pororo/testcode01/MyCalculator.java
+++ src/main/java/com/pororo/testcode01/MyCalculator.java
@@ -1,4 +1,45 @@
 package com.pororo.testcode01;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@AllArgsConstructor
+@Getter
 public class MyCalculator {
+
+    private Double result;
+
+    public MyCalculator() {
+        result = 0.0;
+    }
+
+    public MyCalculator add(Double number){
+        this.result += number;
+        return this;
+    }
+
+    public MyCalculator minus(Double number){
+        this.result -= number;
+        return this;
+    }
+
+    public MyCalculator multiply(Double number){
+        this.result *= number;
+        return this;
+    }
+
+    public MyCalculator divide(Double number){
+
+        if (number == 0.0){
+            throw new ZeroDivisionException();
+        }
+
+        this.result /= number;
+        return this;
+    }
+
+    public static class ZeroDivisionException extends RuntimeException{
+
+    }
+
 }
