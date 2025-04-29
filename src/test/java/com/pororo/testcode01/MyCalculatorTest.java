cc5686117ca012e82c2d2f68ce3f8f2cebe38248
“PororoAndFriends” <gbham0815@pukyong.ac.kr>
2025-04-30 00:18:47 +0900

    계산기 기본 테스트코드 작성

--- src/test/java/com/pororo/testcode01/MyCalculatorTest.java
+++ src/test/java/com/pororo/testcode01/MyCalculatorTest.java
@@ -1,7 +1,6 @@
 package com.pororo.testcode01;
 
-import static org.junit.jupiter.api.Assertions.*;
-
+import com.pororo.testcode01.MyCalculator.ZeroDivisionException;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
@@ -9,21 +8,68 @@ class MyCalculatorTest {
 
     @Test
     void addTest() {
-        MyCalculator myCalculator = new MyCalculator();
+        // AAA 패턴
+
+        // Arrange : 준비
+        MyCalculator myCalculator = new MyCalculator(10.0);
+
+        // Act : 행동
         myCalculator.add(10.0);
 
-        Assertions.assertEquals(10.0, myCalculator.getResult());
+        // Assert : 단언/검증
+        Assertions.assertEquals(20.0, myCalculator.getResult());
     }
 
     @Test
-    void minus() {
+    void minusTest() {
+        // GWT 패턴
+
+        // Given : 준비
+        MyCalculator myCalculator = new MyCalculator(10.0);
+
+        // When : 행동
+        myCalculator.minus(5.0);
+
+        // Then : 단언/검증
+        Assertions.assertEquals(5.0, myCalculator.getResult());
     }
 
     @Test
-    void multiply() {
+    void multiplyTest() {
+        MyCalculator myCalculator = new MyCalculator(2.0);
+        myCalculator.multiply(2.0);
+
+        Assertions.assertEquals(4.0, myCalculator.getResult());
     }
 
     @Test
-    void divide() {
+    void divideTest() {
+        MyCalculator myCalculator = new MyCalculator(4.0);
+        myCalculator.divide(2.0);
+
+        Assertions.assertEquals(2.0, myCalculator.getResult());
     }
+
+    @Test
+    void complicatedCalculateTest() {
+        MyCalculator myCalculator = new MyCalculator(4.0);
+        Double result = myCalculator
+                .add(10.0)
+                .minus(5.0)
+                .multiply(2.0)
+                .divide(2.0)
+                .getResult();
+
+        Assertions.assertEquals(9.0, result);
+    }
+
+    @Test
+    void zeroDivisionTest() {
+        // given
+        MyCalculator myCalculator = new MyCalculator(4.0);
+
+        // when & then
+        Assertions.assertThrows(ZeroDivisionException.class, () -> myCalculator.divide(0.0));
+    }
+
 }
