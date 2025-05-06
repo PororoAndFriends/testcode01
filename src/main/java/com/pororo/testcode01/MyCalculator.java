fdea80b2dd9d8cef3e8aab3fa1083248fdc18eac
“PororoAndFriends” <gbham0815@pukyong.ac.kr>
2025-05-06 18:35:07 +0900

    git hooks 등록을 통한 코드 스타일 검증

--- src/main/java/com/pororo/testcode01/MyCalculator.java
+++ src/main/java/com/pororo/testcode01/MyCalculator.java
@@ -7,38 +7,36 @@ import lombok.Getter;
 @Getter
 public class MyCalculator {
 
-    private Double result;
+  private Double result;
 
-    public MyCalculator() {
-        result = 0.0;
+  public MyCalculator() {
+    result = 0.0;
+  }
+
+  public MyCalculator add(Double number) {
+    this.result += number;
+    return this;
+  }
+
+  public MyCalculator minus(Double number) {
+    this.result -= number;
+    return this;
+  }
+
+  public MyCalculator multiply(Double number) {
+    this.result *= number;
+    return this;
+  }
+
+  public MyCalculator divide(Double number) {
+
+    if (number == 0.0) {
+      throw new ZeroDivisionException();
     }
 
-    public MyCalculator add(Double number){
-        this.result += number;
-        return this;
-    }
+    this.result /= number;
+    return this;
+  }
 
-    public MyCalculator minus(Double number){
-        this.result -= number;
-        return this;
-    }
-
-    public MyCalculator multiply(Double number){
-        this.result *= number;
-        return this;
-    }
-
-    public MyCalculator divide(Double number){
-
-        if (number == 0.0){
-            throw new ZeroDivisionException();
-        }
-
-        this.result /= number;
-        return this;
-    }
-
-    public static class ZeroDivisionException extends RuntimeException{
-    }
-
+  public static class ZeroDivisionException extends RuntimeException {}
 }
